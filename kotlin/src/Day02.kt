733dd97ca35020f1d63e73798ab04297ce442079
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2023-12-02 06:49:04 +0100

    Finished Day 2

--- kotlin/src/Day02.kt
+++ kotlin/src/Day02.kt
@@ -1,9 +1,29 @@
+import kotlin.math.max
+
+val maxVals = mapOf(
+    "red" to 12,
+    "green" to 13,
+    "blue" to 14
+)
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val x = input
+            .map { line -> line.dropWhile { !it.isDigit() }.takeWhile { it.isDigit() }.toInt() to line.dropWhile { it != ':' }.drop(1).split(';') }
+            .filter { p -> p.second.all { it.split(',').all { it.dropWhile { !it.isDigit() }.takeWhile { it.isDigit() }.toInt() <= maxVals[it.dropWhile { !it.isLetter() }]!! } } }
+            .map { it.first }
+            .sum()
+        return x
     }
 
     fun part2(input: List<String>): Int {
+        val x = input
+            .map { line -> line.dropWhile { !it.isDigit() }.takeWhile { it.isDigit() }.toInt() to line.dropWhile { it != ':' }.drop(1).split(';') }
+            .map { line -> line.second.flatMap { it.split(',').map { it.dropWhile { !it.isLetter() } to it.dropWhile { !it.isDigit() }.takeWhile { it.isDigit() }.toInt() } } }
+            .map { line -> line.fold(mutableListOf(0,0,0)) { acc, pair -> if (pair.first == "red") acc[0] = max(acc[0], pair.second) else if (pair.first == "green") acc[1] = max(acc[1], pair.second) else acc[2] = max(acc[2], pair.second); acc } }
+            .map { line -> line.fold(1) { acc, i -> acc * i } }
+        print(x)
+
         return 0
     }
 
