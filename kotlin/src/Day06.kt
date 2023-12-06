90283c9eba93a31f67eaa19453cbe2d3c11fd0e1
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2023-12-06 07:38:03 +0100

    Implement Day6

---
+++ kotlin/src/Day06.kt
@@ -0,0 +1,25 @@
+private fun Pair<Long, Long>.marginOfError() =
+    1 + (0..first).last { it * (first - it) > second } - (0..first).first { it * (first - it) > second }
+
+fun main() {
+    fun part1(input: List<String>): Long = input
+        .map { line ->
+            line.split(' ')
+                .filter { it.any(Char::isDigit) }
+                .map(String::toLong)
+        }
+        .zipWithNext { a, b -> a.zip(b) }
+        .first()
+        .map { it.marginOfError() }
+        .reduce { acc, i -> acc * i }
+
+    fun part2(input: List<String>): Long = input
+        .map { it.filter(Char::isDigit).toLong() }
+        .zipWithNext()
+        .first()
+        .marginOfError()
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+}
