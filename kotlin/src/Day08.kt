b90291882c02e9aaa227536f0568b2f5a77325df
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2023-12-08 21:32:12 +0100

    Improve implementation for Day 8

--- kotlin/src/Day08.kt
+++ kotlin/src/Day08.kt
@@ -1,59 +1,31 @@
 fun main() {
-    fun part1(directions: List<Char>, map: Map<String, Pair<String, String>>): Int {
-        var cur = "AAA"
-        var steps = 0
-        while (cur != "ZZZ") {
-            cur = if (directions[steps%directions.size] == 'L')
-                map[cur]!!.first
-            else
-                map[cur]!!.second
-            steps++
+    fun part1(directions: List<Char>, map: Map<String, Pair<String, String>>): Int =
+        generateSequence(0 to "AAA") {
+            it.first + 1 to (if (directions[it.first % directions.size] == 'L') map[it.second]!!.first else map[it.second]!!.second)
         }
-        return steps
-    }
+            .first { it.second == "ZZZ" }
+            .first
 
-    fun gcd(a: Long, b: Long): Long {
-        var curA = a
-        var curB = b
-        while (curB > 0)
-        {
-            val temp = curB
-            curB = curA % curB
-            curA = temp
-        }
-        return curA
-    }
-
-    fun lcm(a: Long, b: Long): Long {
-        return a * (b / gcd(a, b))
-    }
-
-    fun part2(directions: List<Char>, map: Map<String, Pair<String, String>>): Long {
-        val res = map.filter{ it.key.last() == 'A' }.map {
-            var cur = it.key
-            var steps = 0
-            while (cur.last() != 'Z') {
-                cur = if (directions[steps%directions.size] == 'L')
-                    map[cur]!!.first
+    fun part2(directions: List<Char>, map: Map<String, Pair<String, String>>): Long =
+        generateSequence(map.keys.filter { it.last() == 'A' }.map{ 0 to it }) { gen ->
+            gen.map {
+                if (it.second.last() == 'Z')
+                    it
                 else
-                    map[cur]!!.second
-                steps++
+                    it.first + 1 to (if (directions[it.first % directions.size] == 'L') map[it.second]!!.first else map[it.second]!!.second)
             }
-            steps.toLong()
         }
-
-        var result = res[0]
-        for (x in res.drop(1)) {
-            result = lcm(result, x)
-        }
-
-        return result
-    }
+            .first { it.all { it.second.last() == 'Z' } }
+            .map { it.first.toLong() }
+            .reduce { acc, b -> lcm(acc, b) }
 
     val input = readInput("Day08")
 
     val directions = input.first().toList()
-    val map = input.drop(2).map { s -> Regex("[A-Z]+").findAll(s).map { it.value }.toList() }.filter { it.isNotEmpty() }.associate { it[0] to (it[1] to it[2]) }
+    val map = input.drop(2)
+        .map { s -> Regex("[A-Z]+").findAll(s).map { it.value }.toList() }
+        .filter { it.isNotEmpty() }
+        .associate { it[0] to (it[1] to it[2]) }
 
     println(part1(directions, map))
     println(part2(directions, map))
--- kotlin/src/Util.kt
+++ kotlin/src/Util.kt
@@ -4,3 +4,19 @@ import java.io.File
  * Reads lines from the given resource text file.
  */
 fun readInput(name: String) = File(ClassLoader.getSystemResource(name).path).readLines()
+
+/**
+ * Greatest common divisor (gcd) for two Long values
+ */
+fun gcd(a: Long, b: Long): Long =
+    generateSequence(a to b) {
+        it.second to it.first % it.second
+    }
+        .first { it.second <= 0 }
+        .first
+
+/**
+ * lowest common multiple (lcm) for two Long values
+ */
+fun lcm(a: Long, b: Long): Long =
+    a * (b / gcd(a, b))
