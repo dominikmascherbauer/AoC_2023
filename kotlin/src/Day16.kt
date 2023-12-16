372f679e8ae06267b46a36b382403803661f8ba9
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2023-12-16 14:52:57 +0100

    Cleaner implementation for Day16

--- kotlin/src/Day16.kt
+++ kotlin/src/Day16.kt
@@ -1,99 +1,46 @@
 fun main() {
     fun List<String>.step(dir: Char, x: Int, y: Int): List<Triple<Char,Int,Int>> =
-            when (dir) {
-                'R' -> when (get(y)[x]) {
-                    '\\' -> listOf(Triple('D', x, y + 1))
-                    '/' -> listOf(Triple('U', x, y - 1))
-                    '|' -> listOf(Triple('U', x, y - 1), Triple('D', x, y + 1))
-                    else -> listOf(Triple(dir, x + 1, y))
-                }
-
-                'L' -> when (get(y)[x]) {
-                    '\\' -> listOf(Triple('U', x, y - 1))
-                    '/' -> listOf(Triple('D', x, y + 1))
-                    '|' -> listOf(Triple('U', x, y - 1), Triple('D', x, y + 1))
-                    else -> listOf(Triple(dir, x - 1, y))
-                }
-
-                'U' -> when (get(y)[x]) {
-                    '\\' -> listOf(Triple('L', x - 1, y))
-                    '/' -> listOf(Triple('R', x + 1, y))
-                    '-' -> listOf(Triple('L', x - 1, y), Triple('R', x + 1, y))
-                    else -> listOf(Triple(dir, x, y - 1))
-                }
-
-                'D' -> when (get(y)[x]) {
-                    '\\' -> listOf(Triple('R', x + 1, y))
-                    '/' -> listOf(Triple('L', x - 1, y))
-                    '-' -> listOf(Triple('R', x + 1, y), Triple('L', x - 1, y))
-                    else -> listOf(Triple(dir, x, y + 1))
-                }
-
-                else -> listOf(Triple('0', -1, -1))
+        when (dir to this[y][x]) {
+            'R' to '\\', 'L' to '/' -> listOf(Triple('D', x, y + 1))
+            'R' to '/', 'L' to '\\' -> listOf(Triple('U', x, y - 1))
+            'R' to '|', 'L' to '|' -> listOf(Triple('D', x, y + 1), Triple('U', x, y - 1))
+            'D' to '\\', 'U' to '/' -> listOf(Triple('R', x + 1, y))
+            'D' to '/', 'U' to '\\' -> listOf(Triple('L', x - 1, y))
+            'D' to '-', 'U' to '-' -> listOf(Triple('R', x + 1, y), Triple('L', x - 1, y))
+            else -> when (dir) {
+                'R' -> listOf(Triple(dir, x + 1, y))
+                'L' -> listOf(Triple(dir, x - 1, y))
+                'D' -> listOf(Triple(dir, x, y + 1))
+                'U' -> listOf(Triple(dir, x, y - 1))
+                else -> listOf(Triple('0',-1,-1))
             }
+        }
 
-    fun part1(input: List<String>): Int =
+    fun traverse(grid: List<String>, dir: Char, x: Int, y: Int): Int =
         with(mutableSetOf<Triple<Char,Int,Int>>()) {
-            generateSequence(listOf(Triple('R', 0, 0))) { l ->
+            generateSequence(listOf(Triple(dir, x, y))) { l ->
                 l.onEach { add(it) }
-                    .flatMap { (dir, x, y) -> input.step(dir, x, y) }
-                    .filter { (_, x, y) -> 0 <= x && x < input[0].length && 0 <= y && y < input.size }
+                    .flatMap { (dir, x, y) -> grid.step(dir, x, y) }
+                    .filter { (_, x, y) -> 0 <= x && x < grid[0].length && 0 <= y && y < grid.size }
                     .filterNot { contains(it) }
             }.takeWhile { it.isNotEmpty() }.fold(setOf<Pair<Int, Int>>()) { acc, l ->
                 acc.plus(l.map { (_, x, y) -> x to y }.toSet())
             }.size
         }
 
-    fun part2(input: List<String>): Int {
-        val tmp = List(input[0].length) {
-            with(mutableSetOf<Triple<Char,Int,Int>>()) {
-                generateSequence(listOf(Triple('R', 0, it))) { l ->
-                    l.onEach { add(it) }
-                        .flatMap { (dir, x, y) -> input.step(dir, x, y) }
-                        .filter { (_, x, y) -> 0 <= x && x < input[0].length && 0 <= y && y < input.size }
-                        .filterNot { contains(it) }
-                }.takeWhile { it.isNotEmpty() }.fold(setOf<Pair<Int, Int>>()) { acc, l ->
-                    acc.plus(l.map { (_, x, y) -> x to y }.toSet())
-                }.size
+    fun part1(input: List<String>): Int =
+        traverse(input, 'R', 0, 0)
+
+    fun part2(input: List<String>): Int =
+        List(input[0].length) { it }
+            .flatMap {
+                listOf(traverse(input, 'R', 0, it),
+                    traverse(input, 'L', input[0].length - 1, it),
+                    traverse(input, 'D', it, 0),
+                    traverse(input, 'D', it, input.size - 1)
+                )
             }
-        }.plus(List(input[0].length) {
-            with(mutableSetOf<Triple<Char,Int,Int>>()) {
-                generateSequence(listOf(Triple('L', input[0].length - 1, it))) { l ->
-                    l.onEach { add(it) }
-                        .flatMap { (dir, x, y) -> input.step(dir, x, y) }
-                        .filter { (_, x, y) -> 0 <= x && x < input[0].length && 0 <= y && y < input.size }
-                        .filterNot { contains(it) }
-                }.takeWhile { it.isNotEmpty() }.fold(setOf<Pair<Int, Int>>()) { acc, l ->
-                    acc.plus(l.map { (_, x, y) -> x to y }.toSet())
-                }.size
-            }
-        }).plus(
-            List(input[0].length) {
-                with(mutableSetOf<Triple<Char,Int,Int>>()) {
-                    generateSequence(listOf(Triple('D', it, 0))) { l ->
-                        l.onEach { add(it) }
-                            .flatMap { (dir, x, y) -> input.step(dir, x, y) }
-                            .filter { (_, x, y) -> 0 <= x && x < input[0].length && 0 <= y && y < input.size }
-                            .filterNot { contains(it) }
-                    }.takeWhile { it.isNotEmpty() }.fold(setOf<Pair<Int, Int>>()) { acc, l ->
-                        acc.plus(l.map { (_, x, y) -> x to y }.toSet())
-                    }.size
-                }
-            }
-        ).plus(List(input[0].length) {
-            with(mutableSetOf<Triple<Char,Int,Int>>()) {
-                generateSequence(listOf(Triple('U', it, input.size - 1))) { l ->
-                    l.onEach { add(it) }
-                        .flatMap { (dir, x, y) -> input.step(dir, x, y) }
-                        .filter { (_, x, y) -> 0 <= x && x < input[0].length && 0 <= y && y < input.size }
-                        .filterNot { contains(it) }
-                }.takeWhile { it.isNotEmpty() }.fold(setOf<Pair<Int, Int>>()) { acc, l ->
-                    acc.plus(l.map { (_, x, y) -> x to y }.toSet())
-                }.size
-            }
-        })
-        return tmp.max()
-    }
+            .max()
 
     val input = readInput("Day16")
     println(part1(input))
