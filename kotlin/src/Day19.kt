0739f3fe21acce28ce1e9149cf86e4a7debcdcf1
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2023-12-23 00:07:43 +0100

    Finish Day 19

--- kotlin/src/Day19.kt
+++ kotlin/src/Day19.kt
@@ -1,18 +1,22 @@
-import Part.Companion.parse
-import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.max
 
 private data class Part(val x: Int, val m: Int, val a: Int, val s: Int) {
     constructor(s: String) : this(s.parse('x'), s.parse('m'), s.parse('a'), s.parse('s'))
 
     val sum: Int =
         x + m + a + s
+    fun combinations(other: Part): Long =
+        (other.x - x + 1).toLong() * (other.m - m + 1) * (other.a - a + 1) * (other.s - s + 1)
 
-    companion object {
-        fun String.parse(c: Char): Int =
-            dropWhile { it != c }.drop(2).takeWhile(Char::isDigit).toInt()
-    }
+    fun setX(newX: Int) = Part(newX, m, a, s)
+    fun setM(newM: Int) = Part(x, newM, a, s)
+    fun setA(newA: Int) = Part(x, m, newA, s)
+    fun setS(newS: Int) = Part(x, m, a, newS)
 }
+
+private fun String.parse(c: Char): Int =
+    dropWhile { it != c }.drop(2).takeWhile(Char::isDigit).toInt()
 
 fun main() {
     fun part1(workflows: Map<String, List<String>>, parts: List<Part>): Int =
@@ -38,78 +42,41 @@ fun main() {
             .run { if ("A" in this) part.sum else 0 }
         }
 
-    fun String.combinations(workflows: Map<String, List<String>>, min: Part, max: Part): Pair<Part, Part>? =
-        when (this) {
-            "A" -> min to max
-            "R" -> null
-            else -> {
-                val np: Pair<Part, Part>? = min to max
-                workflows[this]!!.fold(listOf<Pair<Part, Part>?>() to np) { acc, cond ->
-                    if (acc.second == null)
-                        acc
-                    else {
-                        val op = cond.drop(1).firstOrNull()
-                        val variable = cond.first()
-                        val value = cond.drop(2).takeWhile { it.isDigit() }.toIntOrNull()
-                        val target = cond.split(':').last()
-                        when (op) {
-                            '>' -> when (variable) {
-                                'x' -> acc.first.plus(if (value!! > max.x ) null else target.combinations(workflows, Part(max(min.x, value + 1), min.m, min.a, min.s), max)) to target.combinations(workflows, Part(max(min.x, value + 1), min.m, min.a, min.s), max)
-                                'm' -> acc.first.plus(if (value!! > max.m ) null else target.combinations(workflows, Part(min.x, max(min.m, value + 1), min.a, min.s), max)) to null
-                                'a' -> acc.first.plus(if (value!! > max.a ) null else target.combinations(workflows, Part(min.x, min.m, max(min.a, value + 1), min.s), max)) to null
-                                's' -> acc.first.plus(if (value!! > max.s ) null else target.combinations(workflows, Part(min.x, min.m, min.a, max(min.s, value + 1)), max)) to null
-                                else -> acc // should not happen
-                            }
-                            '<' -> when (variable) {
-                                'x' -> acc.first.plus(if (value!! < min.x ) null else target.combinations(workflows, min, Part(min(max.x, value - 1), max.m, max.a, max.s))) to null
-                                'm' -> acc.first.plus(if (value!! < min.m ) null else target.combinations(workflows, min, Part(max.x, min(max.m, value - 1), max.a, max.s))) to null
-                                'a' -> acc.first.plus(if (value!! < min.a ) null else target.combinations(workflows, min, Part(max.x, max.m, min(max.a, value - 1), max.s))) to null
-                                's' -> acc.first.plus(if (value!! < min.s ) null else target.combinations(workflows, min, Part(max.x, max.m, max.a, min(max.s, value - 1)))) to null
-                                else -> acc // should not happen
-                            }
-                            else -> acc.first.plus(cond.combinations(workflows, min, max)) to acc.second
-                        }
+    fun List<String>.combinations(min: Part, max: Part, workflows: Map<String, List<String>>): Long =
+        if (min.x > max.x || min.m > max.m || min.a > max.a || min.s > max.s)
+            0
+        else
+            when (val rule = first()) {
+                "A" -> min.combinations(max)
+                "R" -> 0
+                else -> if (':' !in rule)
+                    workflows[rule]!!.combinations(min, max, workflows)
+                else {
+                    val (minTrue, maxTrue) = when (rule.first()) {
+                        'x' -> if (rule[1] == '<') min to max.setX(min(max.x, rule.parse('x') - 1)) else min.setX(max(min.x, rule.parse('x') + 1)) to max
+                        'm' -> if (rule[1] == '<') min to max.setM(min(max.m, rule.parse('m') - 1)) else min.setM(max(min.m, rule.parse('m') + 1)) to max
+                        'a' -> if (rule[1] == '<') min to max.setA(min(max.a, rule.parse('a') - 1)) else min.setA(max(min.a, rule.parse('a') + 1)) to max
+                        's' -> if (rule[1] == '<') min to max.setS(min(max.s, rule.parse('s') - 1)) else min.setS(max(min.s, rule.parse('s') + 1)) to max
+                        else -> min to max // invalid variable
                     }
-                }.second
-            }
-                    /*
-                    .mapIndexed { i, cond ->
-                    val op = cond.drop(1).firstOrNull()
-                    val variable = cond.first()
-                    val value = cond.drop(2).takeWhile { it.isDigit() }.toIntOrNull()
-                    val target = cond.split(':').last()
-                    when (op) {
-                        '>' -> when (variable) {
-                            'x' -> if (value!! > max.x ) null else target.combinations(workflows, Part(max(min.x, value + 1), min.m, min.a, min.s), max)
-                            'm' -> if (value!! > max.m ) null else target.combinations(workflows, Part(min.x, max(min.m, value + 1), min.a, min.s), max)
-                            'a' -> if (value!! > max.a ) null else target.combinations(workflows, Part(min.x, min.m, max(min.a, value + 1), min.s), max)
-                            's' -> if (value!! > max.s ) null else target.combinations(workflows, Part(min.x, min.m, min.a, max(min.s, value + 1)), max)
-                            else -> null // should not happen
-                        }
-                        '<' -> when (variable) {
-                            'x' -> if (value!! < min.x ) null else target.combinations(workflows, min, Part(min(max.x, value - 1), max.m, max.a, max.s))
-                            'm' -> if (value!! < min.m ) null else target.combinations(workflows, min, Part(max.x, min(max.m, value - 1), max.a, max.s))
-                            'a' -> if (value!! < min.a ) null else target.combinations(workflows, min, Part(max.x, max.m, min(max.a, value - 1), max.s))
-                            's' -> if (value!! < min.s ) null else target.combinations(workflows, min, Part(max.x, max.m, max.a, min(max.s, value - 1)))
-                            else -> null // should not happen
-                        }
-                        else -> cond.combinations(workflows, min, max)
+                    val (minFalse, maxFalse) = when (rule.first()) {
+                        'x' -> if (rule[1] == '>') min to max.setX(min(max.x, rule.parse('x'))) else min.setX(max(min.x, rule.parse('x'))) to max
+                        'm' -> if (rule[1] == '>') min to max.setM(min(max.m, rule.parse('m'))) else min.setM(max(min.m, rule.parse('m'))) to max
+                        'a' -> if (rule[1] == '>') min to max.setA(min(max.a, rule.parse('a'))) else min.setA(max(min.a, rule.parse('a'))) to max
+                        's' -> if (rule[1] == '>') min to max.setS(min(max.s, rule.parse('s'))) else min.setS(max(min.s, rule.parse('s'))) to max
+                        else -> min to max // invalid variable
+                    }
+                    drop(1).combinations(minFalse, maxFalse, workflows) + when (rule.split(':').last()) {
+                        "A" -> minTrue.combinations(maxTrue)
+                        "R" -> 0
+                        else -> workflows[rule.split(':').last()]!!.combinations(minTrue, maxTrue, workflows)
                     }
                 }
-                    .filterNotNull()
-                    .fold(Part(4000,4000,4000,4000) to Part(1,1,1,1)) { acc, next ->
-                        Part(min(acc.first.x, next.first.x), min(acc.first.m, next.first.m), min(acc.first.a, next.first.a), min(acc.first.s, next.first.s)) to
-                        Part(max(acc.second.x, next.second.x), max(acc.second.m, next.second.m), max(acc.second.a, next.second.a), max(acc.second.s, next.second.s))
-                    }
+            }
 
-                     */
-        }
-
-    fun part2(workflows: Map<String, List<String>>): Long {
-        val (min, max) = "in".combinations(workflows, Part(1,1,1,1), Part(4000,4000,4000,4000))!!
-
-        return (max.x - min.x + 1L) * (max.m - min.m + 1L) * (max.a - min.a + 1L) * (max.s - min.s + 1L)
-    }
+    fun part2(workflows: Map<String, List<String>>): Long =
+        workflows["in"]!!.combinations(Part(1,1,1,1), Part(4000,4000,4000,4000), workflows)
+    // 125657431183201 is wrong ... well, turns out it isn't on second try ... (I literally put this same number in twice for it to fail on the first try)
 
     val input = readInput("Day19")
 
@@ -119,5 +86,5 @@ fun main() {
     val parts: List<Part> = input.dropWhile { it.isNotEmpty() }.drop(1).map { Part(it) }
 
     println(part1(workflows, parts))
-    //println(part2(workflows))
+    println(part2(workflows))
 }
