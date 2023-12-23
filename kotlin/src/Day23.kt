3ff45c6ca57c2688d74b7e1bdc6495834c750cd0
dominikmascherbauer <dominik.mascherbauer@gmail.com>
2023-12-23 22:48:02 +0100

    Solve Day 23 (bruteforce)

--- kotlin/src/Day23.kt
+++ kotlin/src/Day23.kt
@@ -1,3 +1,5 @@
+import java.util.Stack
+
 fun main() {
     fun List<List<Point<Char>>>.getNeighbors(p: Point<Char>, isSlippery: Boolean = true): List<Point<Char>> =
         if (isSlippery)
@@ -27,50 +29,100 @@ fun main() {
             .map { (y, x) -> this[y][x] }
             .filter {it.value != '#' }
 
-
     fun part1(input: List<List<Point<Char>>>): Int {
-        val frontier = mutableListOf(0 to input[0][1])
-        val visited = mutableListOf<Pair<Int, Point<Char>>>()
+        val stack: Stack<Point<Char>> = Stack<Point<Char>>()
+        stack.push(input[0][1])
+        val visited = mutableListOf<Point<Char>>()
         val cameFrom = mutableMapOf<Point<Char>, Point<Char>>()
 
-        while (frontier.isNotEmpty()) {
-            val next = frontier.removeFirst()
-            visited.add(next)
-            for (neighbor in input.getNeighbors(next.second)) {
-                val lastVisited = visited.firstOrNull { it.second == neighbor }
-                //if (lastVisited == null || cameFrom[next.second] == lastVisited.second) {
-                    //lastVisited?.first?.add(next.first.first() + 1)
-                    val lastScheduled = frontier.firstOrNull { it.second == neighbor } //?.first?.add(next.first.first() + 1)
+        val paths = mutableListOf<List<Point<Char>>>()
 
-                    if (lastVisited != null && lastVisited.first < next.first - 1) {
-                        visited.remove(lastVisited)
-                        frontier.add((next.first + 1) to neighbor)
-                        cameFrom[neighbor] = next.second
-                    } else if(lastScheduled != null && lastScheduled.first < next.first - 1) {
-                        frontier.remove(lastScheduled)
-                        frontier.add((next.first + 1) to neighbor)
-                        cameFrom[neighbor] = next.second
-                    } else if (visited.none { it.second == neighbor } && frontier.none { it.second == neighbor }) {
-                        frontier.add((next.first + 1) to neighbor)
-                        cameFrom[neighbor] = next.second
+        while (stack.isNotEmpty()) {
+            val next = stack.pop()
+            visited.add(next)
+            if (next.y == input.size - 1) {
+                paths.add(visited.toList())
+                if (stack.isNotEmpty()) {
+                    val unfinishedPath = visited.takeWhile { it != cameFrom[stack.peek()] }
+                    visited.clear()
+                    visited.addAll(unfinishedPath)
+                    visited.add(cameFrom[stack.peek()]!!)
+                }
+            } else {
+                for (neighbor in input.getNeighbors(next)) {
+                    if (visited.none { it == neighbor } && stack.none { it == neighbor }) {
+                        stack.push(neighbor)
+                        cameFrom[neighbor] = next
                     }
-                //}
+                }
             }
         }
+        return paths.maxOf { it.size - 1 }
+    }
 
-        var last = visited.first { it.second.y == input.size - 1 }
-        val path = mutableListOf(last)
-        while (cameFrom[last.second] != null) {
-            last = visited.first { it.second == cameFrom[last.second] }
-            path.add(0, last)
+    fun List<List<Point<Char>>>.getNext(cur: Point<Char>, prev: Point<Char>): List<Point<Char>> {
+        return listOf(
+            cur.y to cur.x + 1,
+            cur.y to cur.x - 1,
+            cur.y + 1 to cur.x,
+            cur.y - 1 to cur.x
+        ).filter { (y, x) -> x >= 0 && y >= 0 && y < size && x < get(0).size }
+            .map { (y, x) -> this[y][x] }
+            .filter {(it.x != prev.x || it.y != prev.y) && it.value != '#' }
+    }
+
+    fun List<List<Point<Char>>>.findNextJunctions(p: Point<Char>, next: List<Point<Char>>): List<Triple<Int, Point<Char>, Point<Char>>> {
+        return next.map {
+            var prev = p
+            var cur = it
+            var steps = 1
+            while (getNext(cur, prev).size == 1) {
+                val tmp = cur
+                cur = getNext(cur, prev).first()
+                prev = tmp
+                steps++
+            }
+            Triple(steps, prev, cur)
         }
-
-        return path.size - 1
-        // 2122 too high
     }
 
     fun part2(input: List<List<Point<Char>>>): Int {
-        return 0
+        val graph: MutableMap<Point<Char>, MutableSet<Pair<Int, Point<Char>>>> = mutableMapOf()
+        val visited: MutableList<Point<Char>> = mutableListOf()
+        val frontier: MutableList<Pair<Point<Char>, List<Point<Char>>>> = mutableListOf(input[0][1] to listOf(input[1][1]))
+
+        while (frontier.isNotEmpty()) {
+            val next = frontier.removeFirst()
+            visited.add(next.first)
+            val nextJunctions = input.findNextJunctions(next.first, next.second)
+            for (junction in nextJunctions) {
+                graph.putIfAbsent(next.first, mutableSetOf())
+                graph.putIfAbsent(junction.third, mutableSetOf())
+                graph[next.first]!!.add(junction.first to junction.third)
+                graph[junction.third]!!.add(junction.first to next.first)
+                if (junction.third !in visited && frontier.none { it.first == junction.third })
+                    frontier.add(junction.third to input.getNext(junction.third, junction.second))
+            }
+        }
+
+        visited.clear()
+        val frontier2: MutableList<Triple<Point<Char>, Int, List<Point<Char>>>> = mutableListOf(Triple(input[0][1], 0, listOf()))
+        val paths = mutableListOf<Int>()
+
+        while (frontier2.isNotEmpty()) {
+            val next = frontier2.removeFirst()
+            if(next.first.y == input.size - 1) {
+                paths.add(next.second)
+            } else {
+                for (neighbor in graph[next.first]!!) {
+                    if (neighbor.second !in next.third && neighbor.second != next.first) {
+                        frontier2.add(Triple(neighbor.second, next.second + neighbor.first, next.third.plus(next.first)))
+                    }
+                }
+            }
+        }
+
+        return paths.max()
     }
 
     val input = readInput("Day23")
